4b2c67bbd7897390d14069cbbfa2b75224f71579
mrbin509 <bineetkumarsahu771@gmail.com>
2025-06-20 03:01:59 +0530

    Updated dreamshelix-cert-generator.py file

--- flask-verifier/app.py
+++ flask-verifier/app.py
@@ -24,6 +24,6 @@ def verify():
     cert_id = request.args.get("cert_id")
     data = sheet.get_all_records()  # moved inside for freshness
     for row in data:
-        if row['Certificate ID'] == cert_id.strip():
+        if row['Certificate ID'] == cert_id:
             return render_template("verified.html", row=row)
     return render_template("not_found.html")
